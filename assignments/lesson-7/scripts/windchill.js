f1a2b1e42a997f3e494d34d8390f909d02476d0c
smilindaisygal <32459288+smilindaisygal@users.noreply.github.com>
2018-02-21 18:37:51 -0600

    most everuthing for lesson 7 is done but not temp

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,1 +1,13 @@
-f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 
+
+var tempF = parsefloat(document.getElementById('Temp').innerHTML);
+var speed = parsefloat(document.getElementById('speed').innerHTML);
+var chill = windChill(tempF, speed);
+
+
+
+function windChill(tempF, speed){
+    var wind = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, .16)) + (0.4275 * tempF * Math.pow(speed, .16));
+    return wind;}
+
+
+    document.getElementById("windChill").innerHTML = chill; 
