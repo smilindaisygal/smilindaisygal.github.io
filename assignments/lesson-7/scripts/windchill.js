b8eb6fb92f1c2f355d3ebf29bf51427cd1f6f6d7
smilindaisygal <32459288+smilindaisygal@users.noreply.github.com>
2018-03-16 10:32:35 -0600

    added lesson 10 to index.html

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,8 +1,9 @@
 
-var high = parseFloat(document.getElementById('high').innerHTML);
-var low = parseFloat(document.getElementById('low').innerHTML);
-var tempF = (high+low) / 2;
-var speed = parseFloat(document.getElementById('speed').innerHTML);
+// var high = parseFloat(document.getElementById('high').innerHTML);
+// var low = parseFloat(document.getElementById('low').innerHTML);
+// var tempF = (high+low) / 2;
+var tempF = parseFloat(document.getElementById('currentTemp').innerHTML);
+var speed = parseFloat(document.getElementById('currentWindspeed').innerHTML);
 var chill = Math.round(35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, .16)) + (0.4275 * tempF * Math.pow(speed, .16)));
 
 document.getElementById("windchill").innerHTML = chill;
