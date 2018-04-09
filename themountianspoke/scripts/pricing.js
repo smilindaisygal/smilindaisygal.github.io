f5d2d1b8058087b9c65ab50472518274ef701817
smilindaisygal <32459288+smilindaisygal@users.noreply.github.com>
2018-04-09 14:46:07 -0600

    getting files into folder

---
+++ themountianspoke/scripts/pricing.js
@@ -0,0 +1,53 @@
+{
+  "Services" : [
+       "Brake Adjustment" : $10,
+       "Brake Bleed" : $25,
+       "Brake Overhaul (coaster)": $35,
+       "Cable Replacemtn" : $15,
+       "Clean Dirty Bike" : $10,
+       "Derailleur Adjustment" : $10,
+       "Drivetrain Clean" : $50,
+       "Fork Services" : $50,
+       "Handle Bar Wrap/Tape": $10,
+       "Headset Adjustment" : $10,
+       "Headset Overhaul" : $25,
+       "Bike Assembly - Basic" : $60,
+       "Bike Assembly - Full Suspension" : $75,
+       "Bike Assembly - High End Road Bike" : $75,
+       "Electric Shift" : $85,
+       "Chain Replacement - Single Speed" : $5,
+       "Chain Replacement - Multi Speed" : $15,
+       "Hub Adjustment - Front" : $8,
+       "Hub Adjustment - Rear" : $10,
+       "Hub Adjustment - Front" : $15,
+       "Hub Adjustment - Rear" : $15,
+       "Shock Service" : $35,
+       "Tire/Tube Replacement" : $6,
+       "Tubular Tire Installation" : $50,
+       "Wheel Building" : $45,
+       "Wheel Truing - basic" : $15,
+       "Wheel Truing - major" : $35,
+  ]
+   },
+      {
+     "Tune-up Packages" : [ 
+          "Big Box Saftey Check" : $20 : "Pedal, axle, headset, and crankset torqued to specification. Brake, 
+            chain, and derailleur adjustment. Tire and wheel check.",
+          "Standard Safety Check" : $10 : "Everything included in the Big Box Safety Check. But on quality named
+             bikes.",
+          "Basic Tune-up" : $75 : " Complete safety check, adjust/torque to specs, shift lub and adjustment,
+            chain clean and adjustment, and light cleaning of bike.",
+          "Race/Event Tune and Detail" : $49 : "Chain lube and clean, frame and wheel detailing, and minor adjustments.",
+          "Standard Tune-up" : $70 : "Wipe Down - frame, derailleurs, fork, wheels; chain clean and lube; brake
+            adjustment; shift adjustment; wheel true; hub adjustment; and head adjustment.
+            Test ridden by certified tech.",
+          "Troy's New Bike Tune-up" : $90 : "Troy's famous for making your bike look and ride like it is new! Standard Tune-up PLUS
+             complete cleaning and waxing of entire bike. Remove and clean all parts and adjust
+             everything to factory specs.",
+          "Overhaul" : "$120 to $250 or more depending on the bike type" : "Brake pad replacement, bearing replacement, cable/housing replacement, suspension
+             analysis and service and other required repairs. Test ridden by certified tech.",    
+           ]
+         }  
+   }
+   
+       
